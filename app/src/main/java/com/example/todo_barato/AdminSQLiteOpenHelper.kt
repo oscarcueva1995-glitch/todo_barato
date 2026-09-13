9fdb8d8023f960695e51a8976bb4609edbd0c85d
osCueva <oscarcueva.1995@gmail.com>
2026-09-12 20:28:33 -0500

    Agregada la clase de conexion a la base de datos AdminSQLiteOpenHelper

---
+++ app/src/main/java/com/example/todo_barato/AdminSQLiteOpenHelper.kt
@@ -0,0 +1,33 @@
+package com.example.todo_barato
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class AdminSQLiteOpenHelper(
+    context: Context?,
+    name: String = "todo_barato_bd",
+    factory: SQLiteDatabase.CursorFactory? = null,
+    version: Int = 1
+) : SQLiteOpenHelper(context, name, factory, version) {
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        val createTableQuery = """
+            CREATE TABLE ventas (
+                codigo TEXT PRIMARY KEY,
+                nombre TEXT,
+                precio REAL,
+                cantidad INTEGER,
+                tipo TEXT,
+                fecha_venta TEXT
+            )
+        """.trimIndent()
+
+        db?.execSQL(createTableQuery)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.execSQL("DROP TABLE IF EXISTS ventas")
+        onCreate(db)
+    }
+}
